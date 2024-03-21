74c08fbc102c4b7d99a63d512ac31f24286e6e18
SAVe1831 <save1831537@gmail.com>
2024-03-21 22:44:16 +0300

    10 commit

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-nowrap justify-content-between border-1 border-gray-300 border-round-2xl p-1 lg:p-2 w-full h-7rem">
         <div class="w-4 flex justify-content-center">
-            <img alt="product photo" src="/images/products/card-darling.jpg" class="h-full w-auto" />
+            <img alt="product photo" src="/images/products/CatDryFood/card-darling.jpg" class="h-full w-auto" />
         </div>
         <div class="w-9 flex flex-column justify-content-between ml-2">
             <p class="w-full m-0 h-4rem overflow-hidden text-xs lg:text-base">Сухой корм для кошек с мясом по-домашнему и овощами. Содержит все необходимые витамины и минеральные вещества.</p>
